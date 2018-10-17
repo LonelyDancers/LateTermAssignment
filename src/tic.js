ce1eea9503388516187abfb87b5a37f8361b594f
vidars17 <vidarsigurds@gmail.com>
2018-10-17 15:31:53 +0000

    Insert complete with tests

--- src/tic.js
+++ src/tic.js
@@ -1,8 +1,29 @@
-var board;
-var xTurn;
+var board=['','','','','','','','',''];
+var	xTurn = true;
+var turnNumber = 1;
+
+function insert(index, isXTurn) {
+	if(index < 0 || index > 8){
+		return false;
+	}
+	if(board[index] != ''){
+		return false;
+	}
+	if(isXTurn){
+		board[index] = 'x';
+	}
+	else{
+		board[index] = 'o';
+	}
+	return true;
+}
+
 function main() {
-	board=['','','','','','','','',''];	
-	xTurn = true;
+
+
 	return "Hello, World!";
 }
-module.exports = main;
+
+module.exports.insert = insert;
+module.exports.main = main;
+module.exports.board = board;
