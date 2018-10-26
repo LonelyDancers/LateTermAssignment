0c6adc5610780de9558798566cf6305232809154
Kolfinna Jónsdóttir <kolfinna17@ru.is>
2018-10-26 02:18:56 +0000

    Added more sophisticated CSS & HTML

--- src/client/index.js
+++ src/client/index.js
@@ -2,14 +2,7 @@ import _ from 'lodash';
 const tic = require("../logic/tic.js");
 const styles = require("../styles/styles.css");
 
-function component() {
-    let element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'there!'], ' ');
-    return element;
-   }
-   document.body.appendChild(component());
-
-   document.getElementById("button").addEventListener("click", function() {
+document.getElementById("button").addEventListener("click", function() {
    tic.insert(3, true);
     var x = tic.getBoard();
     alert(x);
